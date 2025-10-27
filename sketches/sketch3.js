dcec384eea4f8f7dafb4538456133dea4f69972a
lucia-zhy <luciazhy5@gmail.com>
2025-10-27 16:39:50 -0700

    Create the rectangle shape as book first, sketch 3

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,14 +1,22 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function () {
-    p.background(240, 200, 200);
-    p.fill(180, 60, 60);
-    p.textSize(32);
+    p.createCanvas(800, 800);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. B', p.width / 2, p.height / 2);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.draw = function () {
+    // create a rectangle shape as book
+    p.background(255);
+
+    const bookW = 600;
+    const bookH = 450;
+    const x = (p.width - bookW) / 2;
+    const y = (p.height - bookH) / 2;
+
+    p.stroke(0);
+    p.strokeWeight(3);
+    p.fill(255);
+    p.rect(x, y, bookW, bookH, 0);
+  };
 });
