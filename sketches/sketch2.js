099a0c0839592d0a425b5c788dd6b1cf880cfaaf
lucia-zhy <luciazhy5@gmail.com>
2025-10-26 23:28:35 -0700

    Show the current time in the rectangle clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -6,11 +6,21 @@ registerSketch('sk2', function (p) {
   p.draw = function () {
     p.background(255);
     p.fill(255, 220);
+    // create the rectangle shape of the clock
     let rectW = 570;
     let rectH = 420;
     let x = (width - rectW) / 2;
     let y = (height - rectH) / 2;
     p.rect(x, y, rectW, rectH, 10);
+    // show the current time in the rectangle clock
+    let h = hour();
+    let m = minute();
+    let s = second();
+    let timeString = nf(h, 2) + ':' + nf(m, 2) + ':' + nf(s, 2);
+    p.textSize(120);
+    p.fill(0);
+    p.textAlign(CENTER, CENTER);
+    p.text(timeString, width / 2, height / 2);
   };
   p.windowResized = function () { 
     p.resizeCanvas(p.windowWidth, p.windowHeight); 
